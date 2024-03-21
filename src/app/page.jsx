0fe0bed011935617f8959438c4c2a5a7307fbc8e
wcquantech <131394014+wcquantech@users.noreply.github.com>
2024-03-21 13:19:05 +0800

    Implemented fade-in effect for different sections.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,45 @@
+"use client";
 import About from "@/sections/About";
 import Welcome from "@/sections/Welcome";
 import Projects from "@/sections/Projects";
 import Contact from "@/sections/Contact";
 import ImagesModal from "@/components/ImagesModal";
+import { useEffect } from "react";
 
 export default function Home() {
+  // Using Intersection Observer to implement fade in effect
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          entry.target.classList.toggle("show", entry.isIntersecting);
+          if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+    );
+    const sections = document.querySelectorAll(".sections");
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  }, []);
 
   return (
     <div className="flex flex-col">
-      <section id="welcome">
+      <section id="welcome" className="sections">
         <Welcome />
       </section>
 
-      <section id="about">
+      <section id="about" className="sections">
         <About />
       </section>
 
-      <section id="projects">
+      <section id="projects" className="sections">
         <Projects />
       </section>
 
-      <section id="contact">
+      <section id="contact" className="sections">
         <Contact />
       </section>
     </div>
