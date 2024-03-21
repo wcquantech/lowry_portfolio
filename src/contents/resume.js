f811780413eabe054f1e2505141b25def307dc77
wcquantech <131394014+wcquantech@users.noreply.github.com>
2024-03-21 21:00:46 +0800

    Added the resume link and allow users to download at Welcome section

---
+++ src/contents/resume.js
@@ -0,0 +1,6 @@
+const resume = {
+  date: "21/03/2024",
+  url: "https://drive.google.com/uc?export=download&id=1sLmwjnl0miuh63n8fAvBE7lp_7FkKjVu"
+}
+
+export default resume;
--- src/sections/Welcome.jsx
+++ src/sections/Welcome.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import resume from "../contents/resume";
 
 const Welcome = () => {
   return (
@@ -9,7 +10,7 @@ const Welcome = () => {
       <p className="text-2xl sm:text-3xl font-semibold">Full Stack Software Developer.</p>
       {/* Resume and Contact buttons */}
       <div className="flex space-x-4 mt-4">
-        <button className="bg-indigo-600 font-semibold text-sm sm:text-base px-4 py-2 rounded-md text-white border border-gray-200 dark:border-gray-700 hover:bg-indigo-800 hover:text-white active:transform active:scale-90 duration-200">Download Resume</button>
+        <Link href={resume.url} ><button className="bg-indigo-600 font-semibold text-sm sm:text-base px-4 py-2 rounded-md text-white border border-gray-200 dark:border-gray-700 hover:bg-indigo-800 hover:text-white active:transform active:scale-90 duration-200">Download Resume</button></Link>
         <Link href={"/#contact"}><button className="bg-white dark:bg-slate-800 font-semibold text-sm sm:text-base px-4 py-2 rounded-md border border-gray-200 dark:border-gray-700 hover:bg-slate-200 dark:hover:bg-slate-700 active:transform active:scale-90 duration-200">Contact Me!</button></Link>
       </div>
     </div>
